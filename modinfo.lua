ed7f0ceec43e5f2831578e7347cb219656d57a57
HimekaidouHatate <dudals191@naver.com>
2017-04-15 03:11:26 +0900

    Initilizing

---
+++ modinfo.lua
@@ -0,0 +1,44 @@
+name = "Yakumo Yukari"
+description = "Yakumo Yukari comes from unknown world to manipulate Don't Starve Together world!"
+author = "Yakumo Yukari"
+version = "0.1"
+forumthread = ""
+api_version = 10
+
+dst_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+all_clients_require_mod = true 
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+server_filter_tags = {
+	"character",
+}
+
+configuration_options = {
+	{
+		name = "difficulty",
+		label = "Play Style",
+		options =
+		{
+			{ description = "PVP", data = "pvp" },
+			{ description = "Standard", data = "default" },
+			{ description = "Farmer", data = "hard" },
+		},
+		default = "default",
+	},
+	
+	{
+		name = "inspect",
+		label = "Inspect Shadow Creatures",
+		options = 
+		{
+			{ description = "true", data = true },
+			{ description = "false", data = false },
+		},
+		default = true,
+	}
+
+}
