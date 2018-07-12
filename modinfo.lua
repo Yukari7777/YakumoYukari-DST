21c4286b74e935a2e69f4a714df07297db35a1d0
HimekaidouHatate <HimekaidouHatate@users.noreply.github.com>
2018-07-13 00:24:45 +0900

    modinfo

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "Yakumo Yukari"
 description = "Yakumo Yukari comes from unknown world to manipulate Don't Starve Together world!"
 author = "Yakumo Yukari"
-version = "0.6"
+version = "0.6.1"
 forumthread = ""
 api_version = 6
 api_version_dst = 10
