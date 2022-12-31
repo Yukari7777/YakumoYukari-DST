e22e073cbcdb53361615d705aabc590f72faaf0f
Yukari <16526080+Yukari7777@users.noreply.github.com>
2022-12-31 21:44:26 +0900

    String change

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,6 @@
 name = "Yakumo Yukari"
 author = "Yakumo Yukari"
-version = "0.12.05"
+version = "0.12.06"
 description = "Yakumo Yukari comes from unknown world to manipulate Don't Starve Together world!\n\nVersion : "..version.."\nPress [V] to show status.\nYOU MUST ENABLE SCHEME OR YOU'LL GET CRASHED."
 forumthread = ""
 api_version = 6
